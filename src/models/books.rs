324b72195bd70994919f9e89a609e7bfa3bab561
Tony Kwong <tonykkn4321@gmail.com>
2025-08-25 16:12:27 +0800

    Modified every files

--- src/models/books.rs
+++ src/models/books.rs
@@ -2,7 +2,8 @@ use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, Clone)]
 pub struct Book {
-    pub id: i32, // Changed from Uuid to i32
+    pub id: i32,           // Use i32 for ID
     pub title: String,
-    pub author_id: i32, // Changed from Uuid to i32
+    pub year: i32,
+    pub author_id: i32,    // Use i32 for author ID
 }
