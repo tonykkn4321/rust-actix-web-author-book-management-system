33187c8daba8d3a4832b95eed7ce234a203cd078
Tony Kwong <tonykkn4321@gmail.com>
2025-08-25 16:35:27 +0800

    Start all over with only authors table

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,34 @@
 use actix_web::{web, App, HttpServer, HttpResponse};
-use sqlx::{PgPool, MySqlPool};
+use sqlx::{MySqlPool};
 
 mod config;
 mod models;
-use models::{authors::Author, db::DbPool};
+use models::authors::Author;
 
-async fn get_authors(pool: &DbPool) -> HttpResponse {
-    match pool {
-        DbPool::Postgres(pg_pool) => {
-            match sqlx::query_as::<_, Author>("SELECT * FROM authors")
-                .fetch_all(pg_pool)
-                .await
-            {
-                Ok(authors) => HttpResponse::Ok().json(authors),
-                Err(err) => {
-                    eprintln!("Error fetching authors: {:?}", err);
-                    HttpResponse::InternalServerError().finish()
-                }
-            }
-        }
-        DbPool::MySql(mysql_pool) => {
-            match sqlx::query_as::<_, Author>("SELECT * FROM authors")
-                .fetch_all(mysql_pool)
-                .await
-            {
-                Ok(authors) => HttpResponse::Ok().json(authors),
-                Err(err) => {
-                    eprintln!("Error fetching authors: {:?}", err);
-                    HttpResponse::InternalServerError().finish()
-                }
-            }
+async fn get_authors(pool: web::Data<MySqlPool>) -> HttpResponse {
+    match sqlx::query_as::<_, Author>("SELECT * FROM authors")
+        .fetch_all(pool.get_ref())
+        .await
+    {
+        Ok(authors) => HttpResponse::Ok().json(authors),
+        Err(err) => {
+            eprintln!("Error fetching authors: {:?}", err);
+            HttpResponse::InternalServerError().finish()
         }
     }
 }
 
-async fn create_author(pool: &DbPool, new_author: web::Json<Author>) -> HttpResponse {
-    match pool {
-        DbPool::Postgres(pg_pool) => {
-            match sqlx::query("INSERT INTO authors (first_name, last_name) VALUES (?, ?)")
-                .bind(&new_author.first_name)
-                .bind(&new_author.last_name)
-                .execute(pg_pool)
-                .await
-            {
-                Ok(_) => HttpResponse::Created().finish(),
-                Err(err) => {
-                    eprintln!("Error creating author: {:?}", err);
-                    HttpResponse::InternalServerError().finish()
-                }
-            }
-        }
-        DbPool::MySql(mysql_pool) => {
-            match sqlx::query("INSERT INTO authors (first_name, last_name) VALUES (?, ?)")
-                .bind(&new_author.first_name)
-                .bind(&new_author.last_name)
-                .execute(mysql_pool)
-                .await
-            {
-                Ok(_) => HttpResponse::Created().finish(),
-                Err(err) => {
-                    eprintln!("Error creating author: {:?}", err);
-                    HttpResponse::InternalServerError().finish()
-                }
-            }
+async fn create_author(pool: web::Data<MySqlPool>, new_author: web::Json<Author>) -> HttpResponse {
+    match sqlx::query("INSERT INTO authors (first_name, last_name) VALUES (?, ?)")
+        .bind(&new_author.first_name)
+        .bind(&new_author.last_name)
+        .execute(pool.get_ref())
+        .await
+    {
+        Ok(_) => HttpResponse::Created().finish(),
+        Err(err) => {
+            eprintln!("Error creating author: {:?}", err);
+            HttpResponse::InternalServerError().finish()
         }
     }
 }
@@ -71,13 +37,7 @@ async fn create_author(pool: &DbPool, new_author: web::Json<Author>) -> HttpResp
 async fn main() -> std::io::Result<()> {
     config::init();
     let database_url = config::get_database_url();
-    let app_env = config::get_app_env();
-
-    let pool: DbPool = if app_env == "production" {
-        DbPool::Postgres(PgPool::connect(&database_url).await.expect("Failed to create PostgreSQL pool"))
-    } else {
-        DbPool::MySql(MySqlPool::connect(&database_url).await.expect("Failed to create MySQL pool"))
-    };
+    let pool = MySqlPool::connect(&database_url).await.expect("Failed to create MySQL pool");
 
     HttpServer::new(move || {
         App::new()
@@ -85,7 +45,7 @@ async fn main() -> std::io::Result<()> {
             .route("/authors/", web::get().to(get_authors))
             .route("/authors/", web::post().to(create_author))
     })
-    .bind("0.0.0.0:8080")? // Binding to all interfaces
+    .bind("0.0.0.0:8080")?
     .run()
     .await
 }
--- src/models/db.rs
+++
@@ -1,7 +0,0 @@
-// src/models/db.rs
-use sqlx::{PgPool, MySqlPool};
-
-pub enum DbPool {
-    Postgres(PgPool),
-    MySql(MySqlPool),
-}
